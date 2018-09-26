5150ff8f3d403e4736cd904e34477aa9ff9eb7b7
Fran Díaz <MrXesCoGH@users.noreply.github.com>
2018-09-26 12:17:53 +0200

    Commented ex 2 & 3

--- P1/dades/sorted_doubles.c
+++ P1/dades/sorted_doubles.c
@@ -3,6 +3,12 @@
 
 int compara(const void *p1, const void *p2){
 
+	/**
+	* The function compara, it has been modified in order to compare the two
+	* elements and returns 1 if the first element is lower than the second or
+	* returns -1 in the opposite case. In other cases it returns 0. 
+	*/
+
 	double *num1, *num2;
 
 	num1 = (double *) p1;
@@ -17,7 +23,12 @@ int compara(const void *p1, const void *p2){
 }
 
 int main(){
-	double *dble;
+	double *dble; //This creates the vector where we'll be storing the doubles.
+
+	/* 
+	* Next, We read the file where there are the doubles
+	* to sort and load them in a char array.
+	*/
 	
 	FILE *fp;
 	char str[100];
@@ -32,8 +43,14 @@ int main(){
 		puts(str);
 	}
 	
-	int MIDA = atoi(str);
-	dble = malloc(MIDA * sizeof(double));
+	int MIDA = atoi(str); //This gets the full size of the char array after the last read.
+	dble = malloc(MIDA * sizeof(double)); // Using the size that We got before We allocate the vector created before.
+
+	/*
+	* Because in the char array there are loaded the numbers in string format they need to 
+	* be transformed to doubles and, by using the atof function, we solve that problem.
+	* After making the atof We store the doubles inside the dynamic vector in the i position.
+	*/
 
 	for(int i = 0;  i < MIDA; i++){
 		if( fgets (str,100,fp) != NULL){
@@ -44,12 +61,19 @@ int main(){
 
 	fclose(fp);
 
+	//Next, using the qsort function, with the vector, the size We got before, the size of doubles list and
+	//a call to the compara function, We sort the doubles stored inside the dynamic vector.
+
 	qsort(dble, MIDA, sizeof(double), compara);
+
+	//After the sort We print the values in the dynamic vector.
 
 	for(int g = 0; g < MIDA; g++){
 		printf("%lf ", dble[g]);
 		printf("\n");
 	}
+	
+	//Finally, We need to free the dynamic memory that was reserved for the vector.
 
 	free(dble);
 
--- P1/dades/sorted_floats.c
+++ P1/dades/sorted_floats.c
@@ -3,6 +3,12 @@
 
 int compara(const void *p1, const void *p2){
 
+	/**
+	* The function compara, it has been modified in order to compare the two
+	* elements and returns 1 if the first element is lower than the second or
+	* returns -1 in the opposite case. In other cases it returns 0. 
+	*/
+
 	float *num1, *num2;
 
 	num1 = (float *) p1;
@@ -17,8 +23,13 @@ int compara(const void *p1, const void *p2){
 }
 
 int main(){
-	float *flo;
+	float *flo; //This creates the vector where we'll be storing the floats.
 	
+	/* 
+	* Next, We read the file where there are the floats
+	* to sort and load them in a char array.
+	*/
+
 	FILE *fp;
 	char str[100];
 
@@ -32,8 +43,14 @@ int main(){
 		puts(str);
 	}
 	
-	float MIDA = atof(str);
-	flo = malloc(MIDA * sizeof(float));
+	float MIDA = atof(str); //This gets the full size of the char array after the last read.
+	flo = malloc(MIDA * sizeof(float)); // Using the size that We got before We allocate the vector created before.
+
+	/*
+	* Because in the char array there are loaded the numbers in string format they need to 
+	* be transformed to floats and, by using the atof function, we solve that problem.
+	* After making the atof We store the floats inside the dynamic vector in the i position.
+	*/
 
 	for(int i = 0;  i < MIDA; i++){
 		if( fgets (str,100,fp) != NULL){
@@ -42,17 +59,22 @@ int main(){
 		flo[i] = atof(str);
 	}
 
-
 	fclose(fp);
+	
+	//Next, using the qsort function, with the vector, the size We got before, the size of floats list and
+	//a call to the compara function, We sort the floats stored inside the dynamic vector.
 
 	qsort(flo, MIDA, sizeof(float), compara);
+
+	//After the sort We print the values in the dynamic vector.
 
 	for(int g = 0; g < MIDA; g++){
 		printf("%f ", flo[g]);
 		printf("\n");
 	}
 
+	//Finally, We need to free the dynamic memory that was reserved for the vector.
+
 	free(flo);
 	
-
 }
