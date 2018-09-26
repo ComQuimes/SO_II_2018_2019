8b481aadb7dec949c937c0f7ec786af97fb4f36e
Fran Díaz <MrXesCoGH@users.noreply.github.com>
2018-09-26 12:47:52 +0200

    Commented ex 4 & 5

--- P1/dades/sorted_by_ascii_strings.c
+++ P1/dades/sorted_by_ascii_strings.c
@@ -10,8 +10,6 @@ int compara(const void *p1, const void *p2){
 		indicates which string is "shorter" and which "longer" using
 		the ascii value of them.
 	*/
-
-	int value;
 	char *str1, *str2;
 
 	str1 = *((char **) p1);
@@ -22,7 +20,12 @@ int compara(const void *p1, const void *p2){
 }
 
 int main(){
-	char **str_matrix;
+	char **str_matrix; //We create a char matrix where We'll store the strings.
+		
+	/*
+	* We need to get the strings stored in the file. By the following process,
+	* We'll get the strings of the files and storing them in an char array.
+	*/
 
 	FILE *fp;
 	char str[100];
@@ -37,29 +40,45 @@ int main(){
 		puts(str);
 	}
 	
-	int MIDA = atoi(str);
-	int i = 0; // an 
+	int MIDA = atoi(str); //This int stores the total number of lines that We read.
+	int i = 0; //This is just a simple counter used to iterate in the following loop.
 	
-	str_matrix = malloc(MIDA * sizeof(char *)); //we make the first malloc using number of lines
+	str_matrix = malloc(MIDA * sizeof(char *)); //We make the first malloc using number of lines stored previously.
 
 	while(fgets(str,MIDA, fp) != NULL && i < MIDA){
-        int h = strlen(str);
-		str[h-1] = '\0'; //turn the \n to 0
-		str_matrix[i] = (char*) malloc(h*sizeof(char)); //this creates a row in the matrix where it'll store the string.
-		strcpy(str_matrix[i],str); //this copy the string inside the row of the matrix of chars
+        //Here it's stored the length of the char array.
+        int h = strlen(str); 								
+		
+		//turn the \n to 0
+		str[h-1] = '\0'; 									
+		
+		//This creates a row in the matrix where it'll store the string.
+		str_matrix[i] = (char*) malloc(h*sizeof(char)); 
+
+		//This copy the string inside the row of the matrix of chars created before.
+		strcpy(str_matrix[i],str);							
+		
 		i++; 
 	}
 
 	fclose(fp);
+
+	//After storing in the matrix the strings, We sort them using the qsort and the compara function We created.
 
 	qsort(str_matrix, MIDA, sizeof(char **), compara);
 
 	for(int g = 0; g < MIDA; g++){
 		printf("%s ", str_matrix[g]);
 		printf("\n");
+
+		/*
+		* After printing the strings, the vector position that stored the string needs
+		* to be freed.
+		*/
         free(str_matrix[g]);
 	}
 
+	// Finally, We free the rest of the allocated positions assigned by the first malloc.
 	free(str_matrix);
 
 }
--- P1/dades/sorted_by_len_strings.c
+++ P1/dades/sorted_by_len_strings.c
@@ -30,8 +30,13 @@ int compara(const void *p1, const void *p2){
 }
 
 int main(){
-	char **str_matrix;
-
+	char **str_matrix;  //We create a char matrix where We'll store the strings.
+	
+	/*
+	* We need to get the strings stored in the file. By the following process,
+	* We'll get the strings of the files and storing them in an char array.
+	*/
+	
 	FILE *fp;
 	char str[100];
 
@@ -45,29 +50,45 @@ int main(){
 		puts(str);
 	}
 	
-	int MIDA = atoi(str);
-	int i = 0; // an 
+	int MIDA = atoi(str); //This int stores the total number of lines that We read.
+	int i = 0; //This is just a simple counter used to iterate in the following loop.
 	
-	str_matrix = malloc(MIDA * sizeof(char *)); //we make the first malloc using number of lines
+	str_matrix = malloc(MIDA * sizeof(char *)); //we make the first malloc using number of lines stored previously
 
 	while(fgets(str,MIDA, fp) != NULL && i < MIDA){
-        int h = strlen(str);
-		str[h-1] = '\0'; //turn the \n to 0
-		str_matrix[i] = (char*) malloc(h*sizeof(char)); //this creates a row in the matrix where it'll store the string.
-		strcpy(str_matrix[i],str); //this copy the string inside the row of the matrix of chars
+        //Here it's stored the length of the string.
+        int h = strlen(str); 								
+		
+		//This removes the \n at the end of the line.
+		str[h-1] = '\0'; 									
+		
+		//this creates a row in the matrix where it'll store the string.
+		str_matrix[i] = (char*) malloc(h*sizeof(char)); 
+
+		//this copy the string inside the row of the matrix of chars
+		strcpy(str_matrix[i],str);							
+		
 		i++; 
 	}
 
 	fclose(fp);
+
+	//After storing in the matrix the strings, We sort them using the qsort and the compara function We created.
 
 	qsort(str_matrix, MIDA, sizeof(char *), compara);
 
 	for(int g = 0; g < MIDA; g++){
 		printf("%s ", str_matrix[g]);
 		printf("\n");
+
+		/*
+		* After printing the strings, the vector position that stored the string needs
+		* to be freed.
+		*/
         free(str_matrix[g]);
 	}
 
+	// Finally, We free the rest of the allocated positions assigned by the first malloc.
 	free(str_matrix);
 
 }
